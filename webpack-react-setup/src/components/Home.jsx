fb5888f9179a74f0551966a9e51648022c9c7f9c
varunTripjack <varun.issrani@tripjack.com>
2024-10-09 09:40:37 +0530

    webpack-react-setup added

---
+++ webpack-react-setup/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+class Home extends React.Component{
+    render(){
+        return (
+            <div>This is a class Component</div>
+
+        )
+    }
+}
+
+export default Home
